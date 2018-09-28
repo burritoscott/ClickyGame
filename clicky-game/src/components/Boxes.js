68adfbc23cf724d52d1fd289055783a0591f4aa5
Burrito Scott <scottchapman@Scotts-MacBook-Pro.local>
2018-09-27 20:49:05 -0700

    started functions to append images to screen, funciton to keep track of score and highscore, created state for boxes

--- clicky-game/src/components/Boxes.js
+++ clicky-game/src/components/Boxes.js
@@ -1,23 +1,42 @@
 import React, { Component } from "react";
 import "../styles/Boxes.css";
 
-class Boxes extends Component {
-  state = {
-    data: [1, 2, 3, 4, 5]
-  };
-  render() {
-    return (
-      <div className="Boxes">
-        <ul>
-          {this.state.data.map(datum => (
-            <li>{datum}</li>
-          ))}
-        </ul>
-       
-      </div>
-    );
-  }
+
+const imageArr = ["../images/boswer.jpeg"]
+
+for(var key in imageArr){
+  var arrIndex = imageArr[key].jpeg();
+  var image = arrIndex.image("<img>")
+
 }
+
+
+class Boxes extends Component {
+
+  state = {
+    imageArr = ["../images/boswer.jpeg"],
+    score = 0,
+    highScore = 0
+  };
+
+  //this will keep track of the score and have a conditional statment if score = highScore - then replace highScore
+scoreCounter = () => {
+
+}
+
+  render() {
+      this.state.imageArr.map(images => {
+        return 
+
+    
+        }
+
+      // <div className="Boxes">
+      // TEST!
+      // </div>
+
+  }//render end }
+}//component end }
 export default Boxes;
 
 //shuffle(this.state.data).map ------
