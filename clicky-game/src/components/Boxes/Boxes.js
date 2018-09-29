886e736ec876c7b43d2260922d7a687d042ccc15
Burrito Scott <scottchapman@Scotts-MacBook-Pro.local>
2018-09-29 13:37:59 -0700

    added the solution to the repo for reference

--- clicky-game/src/components/Boxes/Boxes.js
+++ clicky-game/src/components/Boxes/Boxes.js
@@ -9,18 +9,8 @@ class Boxes extends Component {
     score: 0,
     highScore: 0
   };
-//function to loop through imageArr to append images to img tags, to the index 'root' class
-imageLoop = () => {
-  for(var key in state.imageArr){
-    var imgVal = document.createElement('img').setAttribute("value", +1);
-    var img = imgVal.setAttribute("src", [key]);
-    boxDisplay.appendChild(img);
-  };
-};
-  //this will keep track of the score and have a conditional statment if score = highScore - then replace highScore
-  scoreCounter() {
 
-  };
+  
 
   render() {
     return(
